6df4d3f771d0058c485a64da1e612bc769c169a7
Carlos Cerdas <cacerdasm@estudiantec.cr>
2024-09-24 23:42:49 -0600

    Cambios realizados

--- src/test/scala/BlockGemmTest.scala
+++ src/test/scala/BlockGemmTest.scala
@@ -9,9 +9,9 @@ class BlockGemmTest extends AnyFlatSpec with ChiselScalatestTester {
     test(new BlockGemm(DefaultConfig.gemmConfig)) { dut =>
 
       // Configurar parámetros del control
-      dut.io.ctrl.bits.M_i.poke(4.U)
-      dut.io.ctrl.bits.K_i.poke(4.U)
-      dut.io.ctrl.bits.N_i.poke(4.U)
+      dut.io.ctrl.bits.M_i.poke(32.U)
+      dut.io.ctrl.bits.K_i.poke(32.U)
+      dut.io.ctrl.bits.N_i.poke(32.U)
       dut.io.ctrl.bits.subtraction_constant_i.poke(0.U)
 
       // Habilitar ctrl.valid
@@ -21,9 +21,9 @@ class BlockGemmTest extends AnyFlatSpec with ChiselScalatestTester {
 
       // Generar matrices 2x2 aleatorias
       val random = new Random()
-      val matrixA = Seq.fill(4)(random.nextInt(100).U(32.W))
-      val matrixB = Seq.fill(4)(random.nextInt(100).U(32.W))
-      val matrixC = Seq.fill(4)(0.U(32.W))
+      val matrixA = Seq.fill(32)(random.nextInt(200).U(32.W))
+      val matrixB = Seq.fill(32)(random.nextInt(200).U(32.W))
+      val matrixC = Seq.fill(32)(0.U(32.W))
 
       // Mostrar matrices generadas (solo para depuración)
       println(s"Matrix A: ${matrixA.mkString(", ")}")
@@ -32,7 +32,8 @@ class BlockGemmTest extends AnyFlatSpec with ChiselScalatestTester {
 
       // Calcular el resultado esperado en Scala
       var expectedSum: BigInt = 0
-      for (i <- 0 until 4) {
+      for (i <- 0 until 32) {
+        println(matrixA(i))
         expectedSum += (matrixA(i).litValue * matrixB(i).litValue) + matrixC(i).litValue
       }
 
@@ -42,46 +43,40 @@ class BlockGemmTest extends AnyFlatSpec with ChiselScalatestTester {
       dut.io.data.c_i.valid.poke(true.B)
 
       // Enviar los valores de las matrices
-      for (i <- 0 until 4) {
+      for (i <- 0 until 32) {
         dut.io.data.a_i.bits.poke(matrixA(i))
         dut.io.data.b_i.bits.poke(matrixB(i))
         dut.io.data.c_i.bits.poke(matrixC(i))
         dut.clock.step(1) // Un paso por cada dato enviado
       }
 
-      // Medir tiempo de ejecución
-      val startTime = System.nanoTime()
+      // Contador de ciclos
+      var cycleCount = 0
 
-      // Esperar a que el módulo procese los datos
-      dut.clock.step(10)
-
-      // Medir el tiempo después de la ejecución
-      val endTime = System.nanoTime()
-      val elapsedTime = endTime - startTime
-      println(f"Tiempo de ejecucion: $elapsedTime nanosegundos")
-
-      // Verificar salida
-      if (dut.io.data.d_o.valid.peek().litToBoolean) {
-        val output = dut.io.data.d_o.bits.peek().litValue
-        println(f"Output D: $output%d")
-        println(f"resultado calculado en TB: $expectedSum")
-
-        // Comparar con el valor esperado
-        val error = (output - expectedSum).abs
-        println(f"Error de aproximacion: $error")
-
-        // Verificar si el error es aceptable
-        if (error < 1) {
-          println("Error de aproximacion menor a 1")
-        } else {
-          println("Error de aproximacion mayor al permitido")
-        }
-      } else {
-        println("Output D no es valido aun.")
+      // Esperar a que el módulo procese los datos y contar los ciclos
+      while (dut.io.data.d_o.valid.peek().litToBoolean == false) {
+        dut.clock.step(1)
+        cycleCount += 1
       }
 
-      // Esperar algunos ciclos adicionales si es necesario
-      dut.clock.step(10)
+      // Verificar salida
+      val output = dut.io.data.d_o.bits.peek().litValue
+      println(f"Output D: $output%d")
+      println(f"Resultado calculado en TB: $expectedSum")
+
+      // Comparar con el valor esperado
+      val error = (output - expectedSum).abs
+      println(f"Error de aproximación: $error")
+
+      // Verificar si el error es aceptable
+      if (error < 1) {
+        println("Error de aproximación menor a 1")
+      } else {
+        println("Error de aproximación mayor al permitido")
+      }
+
+      // Imprimir el total de ciclos de reloj
+      println(s"Total de ciclos de reloj: $cycleCount")
     }
   }
 }
